7ab5c7cfbcb14ff43d9c1032dc116a79db075e0b
vapo94b <vapo94b@gmail.com>
2023-10-21 18:17:42 +0200

    New Output about Information

--- recommendation.py
+++ recommendation.py
@@ -1,12 +1,18 @@
 import restaurantData as r
+def show_info(list):
+    info = ["Name: ","Price: ","Rating: ","Street: "]
+    for j in range(len(list)-1):
+        print(info[j]+list[1:len(list)][j])
+    print("_")
+
 print("In welcher Art von Restaurant wollen Sie essen?")
 i = 1
 for wish in r.types:
     print(str(i)+"."+wish)
     i+=1
-numb = input("Wähle eine 1 - 15!")
+numb = input("Wähle eine 1 - 15\n")
 food = r.types
 for j in range(i):
     if food[int(numb)-1]==r.restaurant_data[j-1][0]:
-        print(r.restaurant_data[j-1])
+        show_info(r.restaurant_data[j-1])
 
