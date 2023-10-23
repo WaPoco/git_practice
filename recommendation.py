509bccfbd7cbf25e6e32902995aaaccc17350403
vapo94b <vapo94b@gmail.com>
2023-10-23 16:27:05 +0200

    Input errors added

--- recommendation.py
+++ recommendation.py
@@ -1,18 +1,26 @@
 import restaurantData as r
 def show_info(list):
     info = ["Name: ","Price: ","Rating: ","Street: "]
-    for j in range(len(list)-1):
-        print(info[j]+list[1:len(list)][j])
-    print("_")
+    print("______________")
+    for label, data in zip(info,list[1:len(list)]):
+        print(label+data)
+    print("______________")
 
 print("In welcher Art von Restaurant wollen Sie essen?")
 i = 1
-for wish in r.types:
+for i, wish in enumerate(r.types, 1):
     print(str(i)+"."+wish)
-    i+=1
-numb = input("Wähle eine 1 - 15\n")
-food = r.types
-for j in range(i):
-    if food[int(numb)-1]==r.restaurant_data[j-1][0]:
-        show_info(r.restaurant_data[j-1])
+while True:
+    try:
+        numb = input("Wähle eine Zahl zwischen 1 - 15!\n")
+        if 1<=int(numb) <= 15:
+            break
+        else:
+            print("Die Zahl muss zwischen 1 und 15 liegen!")
+    except ValueError:
+        print("Keine gültige Eingabe!")
 
+food = r.types
+for j in range(len(r.restaurant_data)):
+    if food[int(numb)-1]==r.restaurant_data[j][0]:
+        show_info(r.restaurant_data[j])
